d45a54e6f21c7c103e0e9b73e646171889be727c
Alexander Schillemans <alexander.schillemans@lhs.global>
2021-04-16 16:13:26 +0200

    Included files to MANIFEST

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from distutils.core import setup
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'python-bol-retailer-api',         
   packages = ['bol'],
-  version = '0.1',
+  version = '0.1.1',
   license='GPL-3.0-or-later',
   description = 'Wrapper for the bol.com Retailer API (v5)',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author = 'Alexander Schillemans',
   author_email = 'alexander.schillemans@lhs.global',
   url = 'https://github.com/alexanderlhsglobal/python-bol-retailer-api',
-  download_url = 'https://github.com/alexanderlhsglobal/python-bol-retailer-api/archive/refs/tags/v0.1.tar.gz',
+  download_url = 'https://github.com/alexanderlhsglobal/python-bol-retailer-api/archive/refs/tags/v0.1.1.tar.gz',
   keywords = ['bol.com', 'api'],
   install_requires=[
           'requests',
